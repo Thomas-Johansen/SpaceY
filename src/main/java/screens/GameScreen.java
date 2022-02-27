41f86a475030ed170a061fe6b0e9e78990c79ffe
Thomas <thomassandjoh@hotmail.com>
2022-02-27 21:18:52 +0100

    Fjernet kommentert bort kode

--- src/main/java/screens/GameScreen.java
+++ src/main/java/screens/GameScreen.java
@@ -120,44 +120,6 @@ public class GameScreen implements Screen {
 		renderer.setView(gamecam);
 	}
 	
-	/*
-	public void inputHandler(float deltaTime) {
-		
-		if(Gdx.input.isKeyJustPressed(Input.Keys.W)) {
-			player.Box2DBody.applyLinearImpulse(new Vector2(0,5f), player.Box2DBody.getWorldCenter(), true);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.D) && player.Box2DBody.getLinearVelocity().x <= 2) {
-			player.Box2DBody.applyLinearImpulse(new Vector2(0.1f, 0), player.Box2DBody.getWorldCenter(), true);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.A) && player.Box2DBody.getLinearVelocity().x >= -2) {
-			player.Box2DBody.applyLinearImpulse(new Vector2(-0.1f, 0), player.Box2DBody.getWorldCenter(), true);
-		}
-		
-		//Test player 2 controls
-		if(Gdx.input.isKeyJustPressed(Input.Keys.UP)) {
-			player2.Box2DBody.applyLinearImpulse(new Vector2(0,5f), player.Box2DBody.getWorldCenter(), true);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.RIGHT) && player2.Box2DBody.getLinearVelocity().x <= 2) {
-			player2.Box2DBody.applyLinearImpulse(new Vector2(0.1f, 0), player.Box2DBody.getWorldCenter(), true);
-		}
-		if(Gdx.input.isKeyPressed(Input.Keys.LEFT) && player2.Box2DBody.getLinearVelocity().x >= -2) {
-			player2.Box2DBody.applyLinearImpulse(new Vector2(-0.1f, 0), player.Box2DBody.getWorldCenter(), true);
-		}
-		
-		//Test reverse gravity
-		if(Gdx.input.isKeyJustPressed(Input.Keys.G)) {
-			Vector2 vector2 = world.getGravity();
-			vector2.y = vector2.y * -1;
-			world.setGravity(vector2);
-			player.Box2DBody.applyLinearImpulse(new Vector2(-0.01f, 0), player.Box2DBody.getWorldCenter(), true);
-			player2.Box2DBody.applyLinearImpulse(new Vector2(-0.01f, 0), player.Box2DBody.getWorldCenter(), true);
-			
-		}
-	
-	}
-
-	*/
-	
 	@Override
 	public void render(float delta) {
 		update(delta);
