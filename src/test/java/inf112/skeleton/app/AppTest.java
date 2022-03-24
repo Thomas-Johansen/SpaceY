90cfb98673f4b69dd9b5bed567988abd2d865d25
Thomas.S.Johansen <thomas.s.johansen@student.uib.no>
2022-03-24 21:09:33 +0100

    Små endringer i kommentarer

--- src/test/java/inf112/skeleton/app/AppTest.java
+++ src/test/java/inf112/skeleton/app/AppTest.java
@@ -2,10 +2,15 @@ package inf112.skeleton.app;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.awt.AWTException;
+import java.awt.Robot;
+import java.awt.event.KeyEvent;
+
 import org.junit.jupiter.api.*;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvSource;
 
+import com.badlogic.gdx.Input;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfiguration;
 
@@ -26,14 +31,16 @@ public class AppTest {
 
 	/**
 	 * Simple test case to see if a test can start the application
+	 * 
 	 */
 	@Test
-	void test1() {
+	void test1() throws AWTException, InterruptedException {
 		 Lwjgl3ApplicationConfiguration cfg = new Lwjgl3ApplicationConfiguration();
 	        
 	        cfg.setTitle("GameTest");
 	        cfg.setWindowedMode(PlatformGame.V_Width, PlatformGame.V_Height);
 	        new Lwjgl3Application(new TestGame(), cfg);
-	}
 
+	}
+	
 }
