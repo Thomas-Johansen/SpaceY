64c1b33686eb53db82c141ed44a2bc6e9bf8dfa1
Daniel.Sandhaug <daniel.sandhaug@student.uib.no>
2022-04-07 14:53:49 +0200

    tester enemy

--- src/main/java/Objects/Enemy.java
+++ src/main/java/Objects/Enemy.java
@@ -3,6 +3,7 @@ package Objects;
 import com.badlogic.gdx.graphics.Texture;
 import com.badlogic.gdx.graphics.g2d.Sprite;
 import com.badlogic.gdx.graphics.g2d.TextureRegion;
+import com.badlogic.gdx.math.Vector2;
 import com.badlogic.gdx.physics.box2d.Body;
 import com.badlogic.gdx.physics.box2d.BodyDef;
 import com.badlogic.gdx.physics.box2d.CircleShape;
@@ -17,16 +18,11 @@ public abstract class Enemy extends Sprite{
     public World world;
     public Body Box2DBody;
 
-    public Enemy(World world, Texture texture, float x, float y) {
+    public Enemy(World world, Texture texture, Vector2 spawn) {
         this.world = world;
         this.texture = texture;
-        setPosition(x,y);
         setBounds(0, 0, 14 / PlatformGame.PPM, 20 / PlatformGame.PPM);
         setRegion(texture);
-    }
-
-    public Enemy(World world, Texture texture) {
-
     }
 
 
