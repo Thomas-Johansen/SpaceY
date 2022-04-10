0d2837ef1b4d144137b8e8d10912bcf4307b589b
Thomas.Lande <thomas.lande@student.uib.no>
2022-04-10 23:30:20 +0200

    Test for gamewindow / MainMenu size

---
+++ src/test/java/inf112/skeleton/app/MainMenuTest.java
@@ -0,0 +1,17 @@
+package inf112.skeleton.app;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class MainMenuTest {
+
+    @Test
+    public void MainMenuHeightTest(){
+        assertEquals(370, PlatformGame.V_Height);
+    }
+
+    @Test
+    public void MainMenuWidthTest(){
+        assertEquals(600, PlatformGame.V_Width);
+    }
+}
